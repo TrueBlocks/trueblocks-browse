2e2b7477c1fbe47ccbc9e9448eb392805ccfa9a3
Thomas Jay Rush <jrush@quickblocks.io>
2024-01-29 05:47:27 -0500

    Adds keyboard control

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 import { GetBlock } from "../wailsjs/go/app/App";
 import { app } from "../wailsjs/go/models";
 import "./App.css";
@@ -6,6 +7,31 @@ import "./App.css";
 function App() {
   const [block, setBlock] = useState<app.Block>();
   const [curBlock, setCurBlock] = useState<number>(1000);
+
+  useHotkeys("home", (event) => {
+    event.preventDefault();
+    setCurBlock(0);
+  });
+  useHotkeys("left", (event) => {
+    event.preventDefault();
+    setCurBlock(curBlock - 1 < 0 ? 0 : curBlock - 1);
+  });
+  useHotkeys("up", (event) => {
+    event.preventDefault();
+    setCurBlock(curBlock - 1000 < 0 ? 0 : curBlock - 1000);
+  });
+  useHotkeys("right", (event) => {
+    event.preventDefault();
+    setCurBlock(curBlock + 1 > 19100000 ? 0 : curBlock + 1);
+  });
+  useHotkeys("down", (event) => {
+    event.preventDefault();
+    setCurBlock(curBlock + 1000 > 19100000 ? 0 : curBlock + 1000);
+  });
+  useHotkeys("end", (event) => {
+    event.preventDefault();
+    setCurBlock(19000000);
+  });
 
   useEffect(() => {
     GetBlock(curBlock).then((val: app.Block) => setBlock(val));
