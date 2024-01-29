7d3cde7e2174a7423680b993494c69e522eb14d5
Thomas Jay Rush <jrush@quickblocks.io>
2024-01-29 05:35:12 -0500

    Removes stuff in preparation to use GetBlock instead of Greet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,25 @@
 import React, { useState } from "react";
 import { Greet } from "../wailsjs/go/app/App";
-import logo from "./assets/images/logo-universal.png";
 import "./App.css";
 
 function App() {
   const [resultText, setResultText] = useState(
     "Please enter your name below 👇"
   );
-  const [name, setName] = useState("");
-  const updateName = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setName(e.target.value);
+  const [name] = useState("Thomas");
   const updateResultText = (result: string) => setResultText(result);
-
   function greet() {
     Greet(name).then(updateResultText);
   }
 
   return (
     <div id="App">
-      <img src={logo} id="logo" alt="logo" />
       <div id="result" className="result">
         {resultText}
       </div>
-      <div id="input" className="input-box">
-        <input
-          id="name"
-          className="input"
-          onChange={updateName}
-          autoComplete="off"
-          name="input"
-          type="text"
-        />
-        <button className="btn" onClick={greet}>
-          Greet
-        </button>
-      </div>
+      <button className="btn" onClick={greet}>
+        Greet
+      </button>
     </div>
   );
 }
