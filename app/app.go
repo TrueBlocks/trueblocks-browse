e4612751492bbdbbf744a24503f0a779e18cc1c9
Thomas Jay Rush <jrush@quickblocks.io>
2024-07-08 21:26:22 -0400

    Names view added

--- app/app.go
+++ app/app.go
@@ -4,12 +4,15 @@ import (
 	"context"
 
 	"github.com/TrueBlocks/trueblocks-browse/pkg/config"
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/logger"
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/types"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 type App struct {
 	ctx     context.Context
 	session config.Session
+	names   []types.Name
 }
 
 func NewApp() *App {
@@ -19,6 +22,9 @@ func NewApp() *App {
 
 func (a *App) Startup(ctx context.Context) {
 	a.ctx = ctx
+	if err := a.loadNames(); err != nil {
+		logger.Panic(err)
+	}
 }
 
 func (a *App) DomReady(ctx context.Context) {
--- frontend/src/components/global/Routes.tsx
+++ frontend/src/components/global/Routes.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Route, Switch } from "wouter";
 import classes from "./Routes.module.css";
+import BlocksView from "@/views/Blocks/BlocksView";
 import HomeView from "@/views/Home/HomeView";
+import NamesView from "@/views/Names/NamesView";
 import SettingsView from "@/views/Settings/SettingsView";
 
 function Routes() {
@@ -9,6 +11,12 @@ function Routes() {
     <div className={classes.container}>
       <Switch>
         {/* Settings */}
+        <Route path="/blocks">
+          <BlocksView />
+        </Route>
+        <Route path="/names">
+          <NamesView />
+        </Route>
         <Route path="/settings">
           <SettingsView />
         </Route>
