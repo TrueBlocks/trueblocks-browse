1021f2f776117b817c94736fc6486a371b73dd3b
Thomas Jay Rush <jrush@quickblocks.io>
2024-10-10 22:28:44 -0400

    Cleaning

--- frontend/src/components/buttons/ViewButton.tsx
+++ frontend/src/components/buttons/ViewButton.tsx
@@ -1,17 +1,20 @@
 import { IconLink } from "@tabler/icons-react";
 import { BaseButton, ButtonProps } from "@components";
-import { SetSessionVal } from "@gocode/app/App";
-import { messages } from "@gocode/models";
+import { SetSessionVal, Reload } from "@gocode/app/App";
+import { base, messages } from "@gocode/models";
 import { EventsEmit } from "@runtime";
 
 // ViewButton opens the history page for a given address.
 export const ViewButton = ({ value, ...props }: ButtonProps) => {
+  const givenAddress = value as base.Address;
   const icon = <IconLink />;
 
   const handleClick = () => {
     SetSessionVal("route", `/history/${value}`);
-    EventsEmit(messages.Message.NAVIGATE, {
-      route: `/history/${value}`,
+    Reload(givenAddress).then(() => {
+      EventsEmit(messages.Message.NAVIGATE, {
+        route: `/history/${value}`,
+      });
     });
   };
 
