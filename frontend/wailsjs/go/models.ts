b97be068e1c975615327db74e930879a3fbf34a2
Thomas Jay Rush <jrush@quickblocks.io>
2024-10-23 21:43:30 -0400

    Cleaning

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -2198,12 +2198,6 @@ export namespace version {
 
 export namespace wizard {
 	
-	export enum Step {
-	    RESET = "Reset",
-	    PREVIOUS = "Previous",
-	    NEXT = "Next",
-	    FINISH = "Finish",
-	}
 	export enum State {
 	    WELCOME = "welcome",
 	    TOMLOKAY = "tomlOkay",
@@ -2211,6 +2205,12 @@ export namespace wizard {
 	    BLOOMSOKAY = "bloomsOkay",
 	    INDEXOKAY = "indexOkay",
 	    OKAY = "okay",
+	}
+	export enum Step {
+	    RESET = "Reset",
+	    PREVIOUS = "Previous",
+	    NEXT = "Next",
+	    FINISH = "Finish",
 	}
 	export class Wizard {
 	    state: State;
