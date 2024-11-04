21f1f9aed825b64e7cc0e63b91d496eca3f840cc
Thomas Jay Rush <jrush@quickblocks.io>
2024-11-04 18:01:03 -0500

    Adds stubs for ConfigPage, WizardPage, and DaemonPage (unused)

---
+++ app/data_config.go
@@ -0,0 +1,7 @@
+package app
+
+import "github.com/TrueBlocks/trueblocks-browse/pkg/types"
+
+func (a *App) ConfigPage(first, pageSize int) *types.ConfigContainer {
+	return nil
+}
---
+++ app/data_wizard.go
@@ -0,0 +1,7 @@
+package app
+
+import "github.com/TrueBlocks/trueblocks-browse/pkg/types"
+
+func (a *App) WizardPage(first, pageSize int) *types.WizardContainer {
+	return nil
+}
