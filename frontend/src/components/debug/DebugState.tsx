6faa546345ae4fd501dc4fe9cd2e91bd1d297cfd
Thomas Jay Rush <jrush@quickblocks.io>
2024-11-09 00:26:21 +0700

    Better state debugger

--- frontend/src/components/debug/DebugState.tsx
+++ frontend/src/components/debug/DebugState.tsx
@@ -1,10 +1,13 @@
 import { Text } from "@mantine/core";
 import { useRenderCounter } from "../../hooks";
+import { useViewState } from "../../state";
 
 export const DebugState = ({ n }: { n: number }) => {
+  const { nItems } = useViewState();
   const renderCount = useRenderCounter();
   return (
     <div>
+      <Text>nItems: {nItems}</Text>
       <Text>lastUpdate: {n}</Text>
       <Text>renderCount: {renderCount}</Text>
     </div>
