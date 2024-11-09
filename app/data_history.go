ab3876ae7d12cb548f13d0b0f7c753e860537417
Thomas Jay Rush <jrush@quickblocks.io>
2024-11-10 03:18:56 +0700

    Temporarily disable history fetching

--- app/data_history.go
+++ app/data_history.go
@@ -3,14 +3,11 @@
 package app
 
 import (
-	"sort"
 	"sync"
 	"sync/atomic"
 
-	"github.com/TrueBlocks/trueblocks-browse/pkg/messages"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/base"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/logger"
-	coreTypes "github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/types"
 	sdk "github.com/TrueBlocks/trueblocks-sdk/v3"
 )
 
@@ -53,113 +50,113 @@ func (a *App) loadHistory(address base.Address, wg *sync.WaitGroup, errorChan ch
 		}
 	}()
 
-	if !historyLock.CompareAndSwap(0, 1) {
-		return nil
-	}
-	defer historyLock.CompareAndSwap(1, 0)
+	// 	if !historyLock.CompareAndSwap(0, 1) {
+	// 		return nil
+	// 	}
+	// 	defer historyLock.CompareAndSwap(1, 0)
 
-	// EXISTING_CODE
-	if address.IsZero() {
-		return nil
-	}
-	// EXISTING_CODE
+	// 	// EXISTING_CODE
+	// 	if address.IsZero() {
+	// 		return nil
+	// 	}
+	// 	// EXISTING_CODE
 
-	history, exists := a.historyCache.Load(address)
-	if exists {
-		if !history.NeedsUpdate(a.forceHistory()) {
-			return nil
-		}
-	}
+	// 	history, exists := a.historyCache.Load(address)
+	// 	if exists {
+	// 		if !history.NeedsUpdate(a.forceHistory()) {
+	// 			return nil
+	// 		}
+	// 	}
 
-	_ = errorChan // delint
-	logger.Info("history: ", address.Hex())
-	if err := a.thing(address, 15); err != nil {
-		a.emitAddressErrorMsg(err, address)
-		return err
-	}
+	// 	_ = errorChan // delint
+	// 	logger.Info("history: ", address.Hex())
+	// 	if err := a.thing(address, 15); err != nil {
+	// 		a.emitAddressErrorMsg(err, address)
+	// 		return err
+	// 	}
 
-	return nil
-}
+	// 	return nil
+	// }
 
-func (a *App) forceHistory() (force bool) {
-	// EXISTING_CODE
-	force = a.forceName()
-	// EXISTING_CODE
-	return
-}
+	// func (a *App) forceHistory() (force bool) {
+	// 	// EXISTING_CODE
+	// 	force = a.forceName()
+	// 	// EXISTING_CODE
+	// 	return
+	// }
 
-// EXISTING_CODE
-func (a *App) thing(address base.Address, freq int) error {
-	rCtx := a.registerCtx(address)
-	defer a.unregisterCtx(address)
+	// // EXISTING_CODE
+	// func (a *App) thing(address base.Address, freq int) error {
+	// 	rCtx := a.registerCtx(address)
+	// 	defer a.unregisterCtx(address)
 
-	opts := sdk.ExportOptions{
-		Addrs:     []string{address.Hex()},
-		RenderCtx: rCtx,
-		Globals: sdk.Globals{
-			Cache: true,
-			Ether: true,
-			Chain: a.session.LastChain,
-		},
-	}
+	// 	opts := sdk.ExportOptions{
+	// 		Addrs:     []string{address.Hex()},
+	// 		RenderCtx: rCtx,
+	// 		Globals: sdk.Globals{
+	// 			Cache: true,
+	// 			Ether: true,
+	// 			Chain: a.session.LastChain,
+	// 		},
+	// 	}
 
-	go func() {
-		nItems := a.getHistoryCnt(address)
-		for {
-			select {
-			case model := <-opts.RenderCtx.ModelChan:
-				tx, ok := model.(*coreTypes.Transaction)
-				if !ok {
-					continue
-				}
-				summary, _ := a.historyCache.Load(address)
-				summary.NTotal = nItems
-				summary.Address = address
-				summary.Name = a.namesMap[address].Name
-				summary.Items = append(summary.Items, *tx)
-				if len(summary.Items)%(freq*3) == 0 {
-					sort.Slice(summary.Items, func(i, j int) bool {
-						if summary.Items[i].BlockNumber == summary.Items[j].BlockNumber {
-							return summary.Items[i].TransactionIndex > summary.Items[j].TransactionIndex
-						}
-						return summary.Items[i].BlockNumber > summary.Items[j].BlockNumber
-					})
-					a.emitProgressMsg(messages.Progress, address, len(summary.Items), int(nItems))
-				}
+	// 	go func() {
+	// 		nItems := a.getHistoryCnt(address)
+	// 		for {
+	// 			select {
+	// 			case model := <-opts.RenderCtx.ModelChan:
+	// 				tx, ok := model.(*coreTypes.Transaction)
+	// 				if !ok {
+	// 					continue
+	// 				}
+	// 				summary, _ := a.historyCache.Load(address)
+	// 				summary.NTotal = nItems
+	// 				summary.Address = address
+	// 				summary.Name = a.namesMap[address].Name
+	// 				summary.Items = append(summary.Items, *tx)
+	// 				if len(summary.Items)%(freq*3) == 0 {
+	// 					sort.Slice(summary.Items, func(i, j int) bool {
+	// 						if summary.Items[i].BlockNumber == summary.Items[j].BlockNumber {
+	// 							return summary.Items[i].TransactionIndex > summary.Items[j].TransactionIndex
+	// 						}
+	// 						return summary.Items[i].BlockNumber > summary.Items[j].BlockNumber
+	// 					})
+	// 					a.emitProgressMsg(messages.Progress, address, len(summary.Items), int(nItems))
+	// 				}
 
-				if len(summary.Items) == 0 {
-					a.historyCache.Delete(address)
-				} else {
-					a.historyCache.Store(address, summary)
-				}
+	// 				if len(summary.Items) == 0 {
+	// 					a.historyCache.Delete(address)
+	// 				} else {
+	// 					a.historyCache.Store(address, summary)
+	// 				}
 
-			case err := <-opts.RenderCtx.ErrorChan:
-				a.emitAddressErrorMsg(err, address)
+	// 			case err := <-opts.RenderCtx.ErrorChan:
+	// 				a.emitAddressErrorMsg(err, address)
 
-			default:
-				if opts.RenderCtx.WasCanceled() {
-					return
-				}
-			}
-		}
-	}()
+	// 			default:
+	// 				if opts.RenderCtx.WasCanceled() {
+	// 					return
+	// 				}
+	// 			}
+	// 		}
+	// 	}()
 
-	_, meta, err := opts.Export() // blocks until forever loop above finishes
-	if err != nil {
-		return err
-	}
-	a.meta = *meta
+	// 	_, meta, err := opts.Export() // blocks until forever loop above finishes
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	a.meta = *meta
 
-	history, _ := a.historyCache.Load(address)
-	sort.Slice(history.Items, func(i, j int) bool {
-		if history.Items[i].BlockNumber == history.Items[j].BlockNumber {
-			return history.Items[i].TransactionIndex > history.Items[j].TransactionIndex
-		}
-		return history.Items[i].BlockNumber > history.Items[j].BlockNumber
-	})
-	history.Summarize()
-	a.historyCache.Store(address, history)
-	a.emitProgressMsg(messages.Completed, address, a.txCount(address), a.txCount(address))
+	// 	history, _ := a.historyCache.Load(address)
+	// 	sort.Slice(history.Items, func(i, j int) bool {
+	// 		if history.Items[i].BlockNumber == history.Items[j].BlockNumber {
+	// 			return history.Items[i].TransactionIndex > history.Items[j].TransactionIndex
+	// 		}
+	// 		return history.Items[i].BlockNumber > history.Items[j].BlockNumber
+	// 	})
+	// 	history.Summarize()
+	// 	a.historyCache.Store(address, history)
+	// 	a.emitProgressMsg(messages.Completed, address, a.txCount(address), a.txCount(address))
 	return nil
 }
 
