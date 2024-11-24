1d2c86262424562979120689682f9a61fa646911
Thomas Jay Rush <jrush@quickblocks.io>
2024-11-24 11:55:44 -0500

    Prepares for type aliases to hide core from the app

---
+++ pkg/types/embeddings.go
@@ -0,0 +1,8 @@
+package types
+
+import coreTypes "github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/types"
+
+type Abi = coreTypes.Abi
+type Transaction = coreTypes.Transaction
+type Name = coreTypes.Name
+type Monitor = coreTypes.Monitor
