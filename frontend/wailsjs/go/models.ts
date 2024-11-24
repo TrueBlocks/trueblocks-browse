a14f6f77ce667e1d5c68698ee052d9b604898d39
Thomas Jay Rush <jrush@quickblocks.io>
2024-11-24 13:58:50 -0500

    Cleanings

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -450,6 +450,12 @@ export namespace sdk {
 
 export namespace types {
 	
+	export enum WizStep {
+	    FIRST = "First",
+	    PREVIOUS = "Previous",
+	    NEXT = "Next",
+	    FINISH = "Finish",
+	}
 	export enum DaemonState {
 	    STOPPED = "Stopped",
 	    RUNNING = "Running",
@@ -462,12 +468,6 @@ export namespace types {
 	    BLOOMS = "blooms",
 	    INDEX = "index",
 	    FINISHED = "finished",
-	}
-	export enum WizStep {
-	    FIRST = "First",
-	    PREVIOUS = "Previous",
-	    NEXT = "Next",
-	    FINISH = "Finish",
 	}
 	export class Parameter {
 	    components?: Parameter[];
