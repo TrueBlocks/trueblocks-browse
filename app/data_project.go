ac80a730e56384f79a253f7d4df4d143ef416a68
Thomas Jay Rush <jrush@quickblocks.io>
2024-11-26 07:28:58 -0500

    Removes noSdk from project

--- app/data_project.go
+++ app/data_project.go
@@ -12,6 +12,8 @@ import (
 	"github.com/TrueBlocks/trueblocks-browse/pkg/types"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/base"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/logger"
+	coreTypes "github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/types"
+	sdk "github.com/TrueBlocks/trueblocks-sdk/v3"
 )
 
 // EXISTING_CODE
@@ -43,48 +45,62 @@ func (a *App) loadProject(wg *sync.WaitGroup, errorChan chan error) error {
 	}()
 	logger.InfoBY("Updating needed for project...")
 
-	// EXISTING_CODE
-	_ = errorChan
-	project := []types.HistoryContainer{}
-	// HIST-PROJ
-	a.historyCache.Range(func(_ base.Address, h types.HistoryContainer) bool {
-		project = append(project, h)
-		return true
-	})
-	sort.Slice(project, func(i, j int) bool {
-		return project[i].Address.Hex() < project[j].Address.Hex()
-	})
-	a.project = types.NewProjectContainer(a.getChain(), project)
-	if !a.project.NeedsUpdate() {
-		return nil
+	opts := ProjectOptions{
+		Globals: a.getGlobals(true /* verbose */),
 	}
-
 	// EXISTING_CODE
 	// EXISTING_CODE
-	// do not remove
-	// EXISTING_CODE
-	// EXISTING_CODE
-	a.project.NItems = uint64(len(a.project.Items))
-	a.project.NMonitors = uint64(len(a.monitors.Items))
-	a.project.NNames = uint64(len(a.names.Items))
-	a.project.NAbis = uint64(len(a.abis.Items))
-	a.project.NIndexes = uint64(len(a.indexes.Items))
-	a.project.NManifests = uint64(len(a.manifests.Items))
-	a.project.NCaches = uint64(len(a.status.Caches))
-	a.project.ForEveryItem(func(item *types.HistoryContainer, data any) bool {
-		a.project.HistorySize += uint64(item.SizeOf())
-		return true
-	}, nil)
-	sort.Slice(a.project.Items, func(i, j int) bool {
-		ai := a.project.Items[i].Address
-		aj := a.project.Items[j].Address
-		return ai.Hex() < aj.Hex()
-	})
-	// EXISTING_CODE
-	a.emitLoadingMsg(messages.Loaded, "project")
+	if items, meta, err := opts.ProjectList(); err != nil {
+		if errorChan != nil {
+			errorChan <- err
+		}
+		return err
+	} else if (items == nil) || (len(items) == 0) {
+		// expected outcome
+		a.meta = *meta
+		return nil
+	} else {
+		// EXISTING_CODE
+		items := []types.HistoryContainer{}
+		// HIST-PROJ
+		a.historyCache.Range(func(_ base.Address, h types.HistoryContainer) bool {
+			items = append(items, h)
+			return true
+		})
+		// EXISTING_CODE
+		a.meta = *meta
+		a.project = types.NewProjectContainer(opts.Chain, items)
+		// EXISTING_CODE
+		a.project.NItems = uint64(len(a.project.Items))
+		a.project.NMonitors = uint64(len(a.monitors.Items))
+		a.project.NNames = uint64(len(a.names.Items))
+		a.project.NAbis = uint64(len(a.abis.Items))
+		a.project.NIndexes = uint64(len(a.indexes.Items))
+		a.project.NManifests = uint64(len(a.manifests.Items))
+		a.project.NCaches = uint64(len(a.status.Caches))
+		a.project.ForEveryItem(func(item *types.HistoryContainer, data any) bool {
+			a.project.HistorySize += uint64(item.SizeOf())
+			return true
+		}, nil)
+		sort.Slice(a.project.Items, func(i, j int) bool {
+			return a.project.Items[i].Address.Hex() < a.project.Items[j].Address.Hex()
+		})
+		// EXISTING_CODE
+		a.emitLoadingMsg(messages.Loaded, "project")
+	}
 
 	return nil
 }
 
 // EXISTING_CODE
+type ProjectOptions struct {
+	Globals sdk.Globals
+	Chain   string
+}
+
+func (opts *ProjectOptions) ProjectList() ([]types.HistoryContainer, *coreTypes.MetaData, error) {
+	meta, err := sdk.GetMetaData(namesChain)
+	return []types.HistoryContainer{}, meta, err
+}
+
 // EXISTING_CODE
