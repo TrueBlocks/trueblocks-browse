8c2b06abd3c24005717b61c7a093b7de0248839f
Thomas Jay Rush <jrush@quickblocks.io>
2024-12-02 09:40:35 -0500

    Small cleanings

--- pkg/types/types_containerwizard.go
+++ pkg/types/types_containerwizard.go
@@ -152,75 +152,12 @@ func (s *WizardContainer) ForEveryItem(process EveryWizErrorFn, data any) bool {
 	return true
 }
 
+func (s *WizardContainer) Sort() (err error) {
+	// EXISTING_CODE
+	// TODO: Sorting?
+	// EXISTING_CODE
+	return
+}
+
 // EXISTING_CODE
-type WizError struct {
-	Index  int      `json:"index"`
-	State  WizState `json:"state"`
-	Reason string   `json:"reason"`
-	Error  string   `json:"error"`
-}
-
-func (w *WizError) ToErr() error {
-	return errors.New(w.Error)
-}
-
-func (w *WizError) String() string {
-	bytes, _ := json.Marshal(w)
-	return string(bytes)
-}
-
-type WizState string
-
-const (
-	WizWelcome  WizState = "welcome"
-	WizConfig   WizState = "config"
-	WizRpc      WizState = "rpc"
-	WizBlooms   WizState = "blooms"
-	WizIndex    WizState = "index"
-	WizFinished WizState = "finished"
-)
-
-// String returns the string representation of the WizState.
-func (s WizState) String() string {
-	return string(s)
-}
-
-// AllWizStates - all possible WizStates for the frontend codegen
-var AllWizStates = []struct {
-	Value  WizState `json:"value"`
-	TSName string   `json:"tsName"`
-}{
-	{WizWelcome, "WELCOME"},
-	{WizConfig, "CONFIG"},
-	{WizRpc, "RPC"},
-	{WizBlooms, "BLOOMS"},
-	{WizIndex, "INDEX"},
-	{WizFinished, "FINISHED"},
-}
-
-type WizStep string
-
-const (
-	WizFirst    WizStep = "First"
-	WizPrevious WizStep = "Previous"
-	WizNext     WizStep = "Next"
-	WizFinish   WizStep = "Finish"
-)
-
-// String returns the string representation of the Step.
-func (s WizStep) String() string {
-	return string(s)
-}
-
-// AllSteps - all possible steps for the frontend codegen
-var AllSteps = []struct {
-	Value  WizStep `json:"value"`
-	TSName string  `json:"tsName"`
-}{
-	{WizFirst, "FIRST"},
-	{WizPrevious, "PREVIOUS"},
-	{WizNext, "NEXT"},
-	{WizFinish, "FINISH"},
-}
-
 // EXISTING_CODE
