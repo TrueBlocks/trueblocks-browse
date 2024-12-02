f9148c6f0887ae082ca2c9bf26155eafe68f7b3f
Thomas Jay Rush <jrush@quickblocks.io>
2024-12-02 08:16:45 -0500

    Cleanings

--- frontend/wailsjs/go/app/App.d.ts
+++ frontend/wailsjs/go/app/App.d.ts
@@ -4,7 +4,6 @@ import {menu} from '../models';
 import {base} from '../models';
 import {types} from '../models';
 import {app} from '../models';
-import {configtypes} from '../models';
 import {context} from '../models';
 import {editors} from '../models';
 
