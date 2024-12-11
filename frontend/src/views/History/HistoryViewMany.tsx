6a1de8c85f0b52e7ef8846e363a2389c3ebc3536
Thomas Jay Rush <jrush@quickblocks.io>
2024-12-11 08:53:00 -0500

    Cleanings

--- frontend/src/views/History/HistoryViewMany.tsx
+++ frontend/src/views/History/HistoryViewMany.tsx
@@ -179,4 +179,204 @@ export const HistoryView = () => {
 };
 
 // EXISTING_CODE
+// This file is auto-generated. Edit only code inside
+// of ExistingCode markers (if any).
+// // EXISTING_CODE
+// import { Table } from "@tanstack/react-table";
+// import { ExploreButton, ExportButton, DataTable, FieldGroup, GoogleButton } from "@components";
+// import { types } from "@gocode/models";
+// import { useAppState } from "../../state";
+// // EXISTING_CODE
+
+// export const HistoryFormDef = (table: Table<types.Transaction>): FieldGroup<types.HistoryContainer>[] => {
+//   // EXISTING_CODE
+//   const { info } = useAppState();
+//   // EXISTING_CODE
+//   return [
+//     // EXISTING_CODE
+//     {
+//       label: "DalleDress",
+//       colSpan: 2,
+//       fields: [{ label: "", type: "dalle", accessor: "address" }],
+//     },
+//     {
+//       label: "Transaction Data",
+//       colSpan: 7,
+//       fields: [
+//         { label: "name", type: "address-name-only", accessor: "address" },
+//         { label: "address", type: "address-address-only", accessor: "address" },
+//         { label: "balance", type: "ether", accessor: "balance" },
+//       ],
+//     },
+//     {
+//       label: "Transaction Data",
+//       colSpan: 3,
+//       fields: [
+//         { label: "nTransactions", type: "int", accessor: "nItems" },
+//         { label: "nLogs", type: "int", accessor: "nLogs" },
+//         { label: "nTokens", type: "int", accessor: "nTokens" },
+//         { label: "nErrors", type: "int", accessor: "nErrors" },
+//       ],
+//     },
+//     {
+//       label: "Buttons",
+//       buttons: [
+//         <ExploreButton key={"explore"} value={info.address} />,
+//         <GoogleButton key={"google"} value={info.address} />,
+//         <ExportButton key={"export"} value={info.address} />,
+//       ],
+//     },
+//     {
+//       label: "Transactions",
+//       collapsable: false,
+//       components: [<DataTable<types.Transaction> key={"dataTable"} table={table} loading={false} />],
+//     },
+//     // EXISTING_CODE
+//   ];
+// };
+
+// // EXISTING_CODE
+// // EXISTING_CODE
+// This file is auto-generated. Edit only code inside
+// of ExistingCode markers (if any).
+// // EXISTING_CODE
+// import { createColumnHelper } from "@tanstack/react-table";
+// import { CustomColumnDef, Formatter } from "@components";
+// import { types } from "@gocode/models";
+// // EXISTING_CODE
+
+// const columnHelper = createColumnHelper<types.Transaction>();
+
+// export const HistoryTableDef: CustomColumnDef<types.Transaction, any>[] = [
+//   // EXISTING_CODE
+//   columnHelper.accessor("blockNumber", {
+//     header: () => "TxId",
+//     cell: (info) => {
+//       const { blockNumber, transactionIndex, hash } = info.row.original;
+//       return <Formatter type="appearance" value={`${blockNumber}.${transactionIndex}`} value2={hash} />;
+//     },
+//     meta: { className: "medium cell" },
+//   }),
+//   columnHelper.accessor("timestamp", {
+//     id: "Timestamp",
+//     cell: (info) => <Formatter type="timestamp" value={info.renderValue()} />,
+//     meta: { className: "medium cell" },
+//   }),
+//   columnHelper.accessor("from", {
+//     header: () => "From",
+//     cell: (info) => <Formatter type="address-editor" value={info.renderValue()} />,
+//     meta: { className: "wide cell" },
+//   }),
+//   columnHelper.accessor("to", {
+//     header: () => "To",
+//     cell: (info) => <Formatter type="address-editor" value={info.renderValue()} />,
+//     meta: { className: "wide cell" },
+//   }),
+//   columnHelper.accessor("value", {
+//     header: () => "Ether",
+//     cell: (info) => {
+//       const { value, isError } = info.row.original;
+//       return <Formatter type="ether" value={value} value2={isError} />;
+//     },
+//     meta: { className: "medium cell" },
+//   }),
+//   columnHelper.accessor("gasUsed", {
+//     header: () => "Gas",
+//     cell: (info) => {
+//       const { gasUsed, gasPrice, from } = info.row.original;
+//       const gasCost = gasUsed * gasPrice;
+//       return <Formatter type="gas" value={gasCost} value2={from} />;
+//     },
+//     meta: { className: "medium cell" },
+//   }),
+//   columnHelper.accessor("isError", {
+//     header: () => "isError",
+//     cell: (info) => <Formatter type="error" value={info.renderValue()} />,
+//     meta: { className: "small center cell" },
+//   }),
+//   columnHelper.accessor("input", {
+//     header: () => "Function",
+//     cell: (info) => <Formatter type="text" value={info.renderValue()} />,
+//     meta: { className: "medium center cell" },
+//   }),
+//   // EXISTING_CODE
+// ];
+
+// // EXISTING_CODE
+// // EXISTING_CODE
+// This file is auto-generated. Edit only code inside
+// of ExistingCode markers (if any).
+
+// // EXISTING_CODE
+// import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
+// import { View, FormTable, ViewForm, DebugState } from "@components";
+// import { useNoops } from "@hooks";
+// import { useAppState, ViewStateProvider } from "@state";
+// import { HistoryTableDef, HistoryFormDef } from ".";
+// // EXISTING_CODE
+
+// export const HistoryView = () => {
+//   const { history, fetchHistory } = useAppState();
+//   const { enterNoop, modifyNoop } = useNoops();
+//   const handleEnter = enterNoop;
+//   const handleModify = modifyNoop;
+
+//   // eslint-disable-next-line prefer-const
+//   let customTabs: string[] = [];
+//   // eslint-disable-next-line prefer-const
+//   let customForms: Record<string, JSX.Element> = {};
+//   // EXISTING_CODE
+//   customTabs = [
+//     "balances",
+//     "incoming",
+//     "outgoing",
+//     "internal",
+//     "charts",
+//     "logs",
+//     "statements",
+//     "neighbors",
+//     "traces",
+//     "receipts",
+//   ];
+//   customForms["balances"] = <div>This is the balances tab</div>;
+//   customForms["incoming"] = <div>This is the incoming tab</div>;
+//   customForms["outgoing"] = <div>This is the outgoing tab</div>;
+//   customForms["internal"] = <div>This is the internal tab</div>;
+//   customForms["charts"] = <div>This is the charts tab</div>;
+//   customForms["logs"] = <div>This is the logs tab</div>;
+//   customForms["statements"] = <div>This is the statements tab</div>;
+//   customForms["neighbors"] = <div>This is the neighbors tab</div>;
+//   customForms["traces"] = <div>This is the traces tab</div>;
+//   customForms["receipts"] = <div>This is the receipts tab</div>;
+//   // EXISTING_CODE
+
+//   const table = useReactTable({
+//     data: history?.items || [],
+//     columns: HistoryTableDef,
+//     getCoreRowModel: getCoreRowModel(),
+//   });
+
+//   const tabs = ["history", ...(customTabs || [])];
+//   const forms: ViewForm = {
+//     history: <FormTable data={history} groups={HistoryFormDef(table)} />,
+//     ...customForms,
+//   };
+
+//   return (
+//     <ViewStateProvider
+//       // do not remove - delint
+//       nItems={history.nItems}
+//       fetchFn={fetchHistory}
+//       onEnter={handleEnter}
+//       modifyFn={handleModify}
+//       tabs={tabs}
+//     >
+//       <DebugState u={[history.updater]} />
+//       <View forms={forms} searchable />
+//     </ViewStateProvider>
+//   );
+// };
+
+// // EXISTING_CODE
+// // EXISTING_CODE
 // EXISTING_CODE
