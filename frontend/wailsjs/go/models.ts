fa11ad59e715d5f825ffd56189554ed43fcd5268
Thomas Jay Rush <jrush@quickblocks.io>
2024-12-12 00:12:24 -0500

    Updatings

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -456,11 +456,6 @@ export namespace sdk {
 
 export namespace types {
 	
-	export enum DaemonState {
-	    STOPPED = "Stopped",
-	    RUNNING = "Running",
-	    PAUSED = "Paused",
-	}
 	export enum WizState {
 	    WELCOME = "welcome",
 	    CONFIG = "config",
@@ -474,6 +469,11 @@ export namespace types {
 	    PREVIOUS = "Previous",
 	    NEXT = "Next",
 	    FINISH = "Finish",
+	}
+	export enum DaemonState {
+	    STOPPED = "Stopped",
+	    RUNNING = "Running",
+	    PAUSED = "Paused",
 	}
 	export class Parameter {
 	    components?: Parameter[];
